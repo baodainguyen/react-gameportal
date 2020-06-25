ebd1b008967569e177c7bcb7fe447fad8bc972e7
DaiNB <baodaihd89@gmail.com>
2020-06-25 23:09:24 +0700

    update code

---
+++ build/precache-manifest.0e26554de084d61dfcf18f926407c6eb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd732254982655eeccf82d926c052058",
+    "url": "/react-gameportal/index.html"
+  },
+  {
+    "revision": "03a276cb15838b63f287",
+    "url": "/react-gameportal/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "f5de3b26768bc708f636",
+    "url": "/react-gameportal/static/css/main.263de9d1.chunk.css"
+  },
+  {
+    "revision": "03a276cb15838b63f287",
+    "url": "/react-gameportal/static/js/2.53733f28.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-gameportal/static/js/2.53733f28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5de3b26768bc708f636",
+    "url": "/react-gameportal/static/js/main.f2a7d063.chunk.js"
+  },
+  {
+    "revision": "a90f4f34953858ed0821",
+    "url": "/react-gameportal/static/js/runtime-main.a4e67419.js"
+  },
+  {
+    "revision": "feface72008e6857da27f939e1eceffa",
+    "url": "/react-gameportal/static/media/Proxima-Nova-Regular.feface72.ttf"
+  },
+  {
+    "revision": "72b6f2c9990bd3d37b8013a59ed78902",
+    "url": "/react-gameportal/static/media/Proxima-Nova.72b6f2c9.otf"
+  },
+  {
+    "revision": "036622f2d97877fabece595b0443d198",
+    "url": "/react-gameportal/static/media/Rstar.036622f2.png"
+  },
+  {
+    "revision": "a1672b5c4f341bf44b6f50b5d4f6d870",
+    "url": "/react-gameportal/static/media/gamepromo.a1672b5c.jpg"
+  },
+  {
+    "revision": "91c1b74e61c2167d380a939d121e38ed",
+    "url": "/react-gameportal/static/media/nv.91c1b74e.png"
+  },
+  {
+    "revision": "2bff81105e4d8813df7d77e4c3df080b",
+    "url": "/react-gameportal/static/media/nv2.2bff8110.jpg"
+  },
+  {
+    "revision": "453f4591b506575376cc4a653aaf746a",
+    "url": "/react-gameportal/static/media/nv3.453f4591.jpg"
+  },
+  {
+    "revision": "2493347b659a8f997f1271e04f9cbb0a",
+    "url": "/react-gameportal/static/media/nv4.2493347b.png"
+  },
+  {
+    "revision": "12d97843ee9febd93dd38aa4bc27c730",
+    "url": "/react-gameportal/static/media/streammer.12d97843.jpg"
+  },
+  {
+    "revision": "0cedf97fb18093c8a513327dfc198aa8",
+    "url": "/react-gameportal/static/media/streammer2.0cedf97f.jpg"
+  }
+]);
